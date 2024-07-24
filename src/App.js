0425d3932ec7ac276fc30304c3bdf364d90201a4
Nithish Kumar <nithishbalasubramanianb@gmail.com>
2024-07-24 12:34:59 +0530

    login

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {true ? (
+        {isValidUser ? (
           <>
             <NavBarPanel />
             <Routes>
